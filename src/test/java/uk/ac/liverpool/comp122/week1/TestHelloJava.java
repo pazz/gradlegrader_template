d105e8a256a8160d205f52edb688795086852ae9
Patrick Totzke <totzke@liverpool.ac.uk>
2020-05-23 14:33:23 +0100

    junit5

--- src/test/java/uk/ac/liverpool/comp122/week1/TestHelloJava.java
+++ src/test/java/uk/ac/liverpool/comp122/week1/TestHelloJava.java
@@ -1,19 +1,21 @@
 package uk.ac.liverpool.comp122.week1;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+//import org.junit.jupiter.api.Tag;
 import edu.illinois.cs.cs125.gradlegrader.annotations.Graded;
-//import com.github.cs125-illinois.gradlegrader.annotations.Graded;
+import edu.illinois.cs.cs125.gradlegrader.annotations.Tag;
 
 import java.io.*;
 
 public class TestHelloJava {
 
-  //@Test(timeout = 300)
+  @Test
+  @Timeout(300)  // JUnit5 !
+  //@Tag("slow")  // JUnit5 Tag
   @Graded(points = 10)
-  //@Tag(name = "difficulty", value = "simple")
+  @Tag(name = "difficulty", value = "simple")  // GradleGrader Tag
   public void testHelloWorld() {
     PrintStream originalOut = System.out;
     ByteArrayOutputStream bos = new ByteArrayOutputStream();
