ef37a911eaccf7e0427eef4cc174ec35855dd1d2
Patrick Totzke <totzke@liverpool.ac.uk>
2020-05-23 13:15:21 +0100

    gradlegrader

--- src/test/java/uk/ac/liverpool/comp122/week1/TestHelloJava.java
+++ src/test/java/uk/ac/liverpool/comp122/week1/TestHelloJava.java
@@ -4,12 +4,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 import org.junit.jupiter.api.Test;
+import edu.illinois.cs.cs125.gradlegrader.annotations.Graded;
+//import com.github.cs125-illinois.gradlegrader.annotations.Graded;
 
 import java.io.*;
 
 public class TestHelloJava {
 
-  @Test
+  //@Test(timeout = 300)
+  @Graded(points = 10)
+  //@Tag(name = "difficulty", value = "simple")
   public void testHelloWorld() {
     PrintStream originalOut = System.out;
     ByteArrayOutputStream bos = new ByteArrayOutputStream();
